21501b23dd6f4e2673f870a92c661b7c6f5d98a3
RASOLOFONIAINA Mickael <rasolofoniaina_mickael@outlook.fr>
2024-10-10 19:09:28 +0300

    Add swagger to catalog API

--- Services/Catalog/CatalogAPI/Program.cs
+++ Services/Catalog/CatalogAPI/Program.cs
@@ -7,9 +7,28 @@ builder.Services.AddMediatR(config =>
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
 });
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c => 
+{   
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Catalog API",
+        Version = "v1"
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog API V1");
+        c.RoutePrefix = string.Empty; // Set Swagger UI at the app's root
+    });
+}
 app.MapCarter();
 
 app.Run();
